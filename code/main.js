962c427b65d2747003a3de25f5417a479a9d169a
clamuflage <ianssantana22@gmail.com>
2020-06-27 02:25:00 -0300

    troca de nome da pasta homepage

--- code/main.js
+++
@@ -1,7 +0,0 @@
-const buttonEntrar = document.querySelector(".btn-entrar")
-console.log(buttonEntrar);
-
-buttonEntrar.addEventListener('click', function (){
-    const inputName = document.querySelector(".input-name")
-    localStorage.setItem('nomeUsuario', inputName.value)
-})
